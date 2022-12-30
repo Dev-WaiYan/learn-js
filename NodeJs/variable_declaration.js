d924e2e3c8e52cdb83a80bb611440c79f9c8a047
HsuLaeHannah <hsulaehannah361@gmail.com>
2022-12-30 22:22:12 +0630

    [ENHANCEMENT] add variable_declaration.js

---
+++ NodeJs/variable_declaration.js
@@ -0,0 +1,21 @@
+var v=4; //assign, initialized
+v=5; //override, reassign
+
+console.log(`v is ${v}`);
+
+
+let l=4;
+l=7; //override, reassign
+console.log(`l is ${l}`);
+
+
+const c=1;
+//c=10;
+console.log(`c is ${c}`);
+
+var vi;
+let li;
+const ci=3;
+console.log(`vi is ${vi}`);
+console.log(`li is ${li}`);
+console.log(`ci is ${ci}`);
